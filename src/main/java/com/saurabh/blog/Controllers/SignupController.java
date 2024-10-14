2a529583481d192b335c9df8cceb77617b984a17
saurabhkr28 <saurabhkumarr28@gmail.com>
2024-10-14 20:06:30 +0530

    commit test1

--- src/main/java/com/saurabh/blog/Controllers/SignupController.java
+++ src/main/java/com/saurabh/blog/Controllers/SignupController.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class SignupController {
 
     @Autowired
